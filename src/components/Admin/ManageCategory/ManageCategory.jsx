244e4a5be34d655fa13c48def54a078bc8ce22dd
Abdul Hannan <68936862+hannan-abdul@users.noreply.github.com>
2023-09-26 22:40:22 -0400

    Update ManageCategory.jsx

--- src/components/Admin/ManageCategory/ManageCategory.jsx
+++ src/components/Admin/ManageCategory/ManageCategory.jsx
@@ -18,7 +18,7 @@ const ManageCategory = () => {
     useEffect(() => {
         const getAllNews = async () => {
             try {
-                const res = await axios.get('https://warm-ocean-89697.herokuapp.com/api/categories/all');
+                const res = await axios.get('https://newsportal-server-i4kcjaat3-shohas563-gmailcom.vercel.app/api/categories/all');
                 dispatch(getCategoriesAction(res.data))
             }
             catch (err) {
